62c26034c27489457c0cc51cc4b4a72a8358217d
Igor <igornteixeira48@gmail.com>
2023-04-07 20:18:35 -0300

    Exercicio-2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // Exercício 1
 let OlaGente = undefined
 console.log(OlaGente) //saida
+
+// Exercício 2
+let numero1 = 22
+if (numero1 % 2 == 0) {
+    console.log('Seu número é par')
+} else {
+    console.log('Seu número é ímpar')
+}
