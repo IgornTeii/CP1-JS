742674ff7dc12e52f6ec0682673fc4895dddfb36
Igor <igornteixeira48@gmail.com>
2023-04-07 20:28:33 -0300

    Exercicio-3

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ let OlaGente = undefined
 console.log(OlaGente) //saida
 
 // Exercício 2
-let numero1 = 22
+/*let numero1 = 22
 if (numero1 % 2 == 0) {
     console.log('Seu número é par')
 } else {
     console.log('Seu número é ímpar')
+} */
+
+//Exercicio 3
+let peso = 65
+let altura = 175
+let IMC = peso / (altura * altura)
+
+if (IMC <= 18, 5) {
+    console.log('você está abaixo do peso')
+} else if (IMC >= 18, 5 <= 24, 9) {
+    console.log('você está no peso ideal')
+} else {
+    console.log('você está acima do peso')
 }
